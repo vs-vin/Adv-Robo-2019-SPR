39420cdcce1e31652c7339c3fa8e90acb86afc5a
vs-vin <vinayaks.sharma26@gmail.com>
2019-07-29 18:15:44 +1000

    added all new files to the commit

---
+++ A1.1/bayes_filter/CMakeFiles/bayes_filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bayes_filter.dir/bayes_filter.cpp.o"
+  "bayes_filter.pdb"
+  "bayes_filter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bayes_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
